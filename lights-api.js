110500806ed1c5c255763293250c6c97b27c6adb
Danny Qiu <dqiu55@gmail.com>
2024-11-16 01:40:36 -0800

    Remove Origin header using chrome declarative net request api

--- lights-api.js
+++ lights-api.js
@@ -1,7 +1,24 @@
+/**
+ * Returns a hash code from a string
+ * @param  {String} str The string to hash.
+ * @return {Number}    A 32bit integer
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+function hashCode(str) {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        let chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
 class Light {
     constructor(ip, port) {
         this.ip = ip;
         this.port = port;
+        this.id = Math.abs(hashCode(ip) ^ port);
     }
 
     async #sendRequest(path, method, body="") {
@@ -17,7 +34,8 @@ class Light {
                 body: body,
             });
         }
-        return fetch(req)
+        await registerOriginHeaderRemoval(this.id, url);
+        return fetch(req, {referrerPolicy: "no-referrer"})
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
@@ -51,3 +69,21 @@ class Light {
     }
 }
 
+async function registerOriginHeaderRemoval(id, url) {
+  const rules = [{
+    id: id,
+    action: {
+      type: 'modifyHeaders',
+      requestHeaders: [{ header: 'Origin', operation: 'remove' }],
+    },
+    condition: {
+      requestMethods: ['put'],
+      urlFilter: `|${url}`
+    },
+  }];
+  await chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: rules.map(r => r.id),
+    addRules: rules,
+  });
+  console.log(await chrome.declarativeNetRequest.getDynamicRules());
+}
